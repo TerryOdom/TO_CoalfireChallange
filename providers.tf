407050fba146613d28aea222e82ea5b68b5fb088
Terry Odom <25593917+TerryOdom@users.noreply.github.com>
2025-08-24 16:32:03 -0400

    update

--- providers.tf
+++ providers.tf
@@ -1,14 +1,1 @@
-# Specifies the provider and version requirements
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
-}
 
-# Configures the AWS provider with the specified region
-provider "aws" {
-  region = var.aws_region
-}
